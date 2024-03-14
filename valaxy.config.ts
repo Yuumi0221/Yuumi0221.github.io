8e45af22e746dc0046b0eac33e24b1051bbdcebe
Yuumi <59047663+Yuumi0221@users.noreply.github.com>
2024-03-14 13:26:31 +0800

    Update valaxy.config.ts

--- valaxy.config.ts
+++ valaxy.config.ts
@@ -125,7 +125,7 @@ export default defineValaxyConfig<UserThemeConfig>({
       bgmUid: '836577',
       bilibiliEnabled: true,
       bgmEnabled: true,
-      customCss: '.bbc-bangumi-title a { color: yellow; }'
+      customCss: '.bbc-bangumi-title a { color: red; }'
     }),
   ],
   
